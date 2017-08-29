2b924f92239b3b6cdbdf1b78456692b574cf3ca5
Rasmus Berg Palm <rasmusbergpalm@gmail.com>
2017-08-29 11:16:02 +0200

    Add slack communication channel

--- communicate/slack.py
+++ communicate/slack.py
@@ -5,5 +5,5 @@ from slacker import Slacker
 client = Slacker(os.environ['SLACK_TOKEN'])
 
 
-def send(channel=None, process=None, subject_format='{pid} ended'):
-    client.chat.post_message(channel, subject_format.format(pid=process), username='Process notifier')
+def send(channel=None, process=None, subject_format='{executable} process {pid} ended'):
+    client.chat.post_message(channel, subject_format.format(**process.__dict__), username='Process notifier')
